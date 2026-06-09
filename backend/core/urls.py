ab71bdb1bf794439746fdaf74255364fd2f80156
matimbamaholobela <matimbascarra101@gmail.com>
2026-06-09 14:48:21 +0200

    document auth and add throlittle

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,3 @@
-# backend/core/urls.py
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
--- backend/idea/models.py
+++ backend/idea/models.py
@@ -1,4 +1,3 @@
-# backend/idea/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator, MinValueValidator
